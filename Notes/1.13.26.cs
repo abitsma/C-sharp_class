fef3469e92a48e3e34e8976887c209d9c0a95208
abitsma <168586741+abitsma@users.noreply.github.com>
2026-01-13 14:53:01 -0700

    Notes

---
+++ Notes/1.13.26.cs
@@ -0,0 +1,29 @@
+//This is how you make code comments
+
+/*
+This is 
+a
+multi-line comment
+*/
+
+//There are apprently how to do curly braces in C# to make things aesthetic
+
+//Python uses snakecase
+//C sharp uses something totally different. You just jam all the words together and use uppercase
+// TinyTanks  This is 
+
+using System;
+namespace FirstNotes
+{
+    public class Program
+    {
+        public static void Main() //here you HAVE to have "main" as it's the way that C# works. It's similar to python def Main
+        {
+            
+        }
+    }
+}
+
+//It seems like this is the typical structure for C# code
+
+//There are at least two different types of errors. Compilation errors and rRuntime errors
---
+++ Notes/FirstPrint.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyFirstPrint
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
