718477cba49a439836de9f1af6914b4b91d3516a
abitsma <168586741+abitsma@users.noreply.github.com>
2026-01-14 10:39:55 -0700

    stuffidk

--- Notes/1.13.26.cs
+++ Notes/1.13.26.cs
@@ -19,7 +19,18 @@ namespace FirstNotes
     {
         public static void Main() //here you HAVE to have "main" as it's the way that C# works. It's similar to python def Main
         {
-            
+            //here's how to create a variable in C#
+            int x = 5;
+            //Or like this
+            int y;
+            y = 5;
+            //Datatypes are int, double, string, bool, etc.
+            Console.WriteLine(x + y);
+            Console.WriteLine("The value of x is " + x);
+            //This is how ot get an input from the user
+            Console.Write("What is your favorite color? ");
+            string color = Console.ReadLine();
+            Console.WriteLine(color);
         }
     }
 }
