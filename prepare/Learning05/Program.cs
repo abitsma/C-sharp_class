16ca84eb592946bf155de9e19f2a5488fea8822c
abitsma <168586741+abitsma@users.noreply.github.com>
2026-03-11 19:14:59 -0600

    sean

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Buffers;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square squar = new Square(4, "yellow");
+        Rectangle rect = new Rectangle(2, 4, "blue");
+        Circle circ = new Circle(10, "red");
+        List<Shape> lista = new List<Shape>();
+        lista.Add(squar);
+        lista.Add(rect);
+        lista.Add(circ);
+
+        foreach (Shape shape in lista)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
